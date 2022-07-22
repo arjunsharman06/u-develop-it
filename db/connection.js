c3d39931345b35dda8c90608abf02bdcaadec5be
ARJUN <arjunsharman06@gmail.com>
2022-07-22 12:25:02 -0400

    added votes table

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ const db = mysql.createConnection(
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'sharman18',
+      password: '',
       database: 'election'
     },
     console.log('Connected to the election database.')
